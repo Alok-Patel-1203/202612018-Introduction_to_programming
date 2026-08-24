503bd5f89de66749025576b31d75881726b3fa4a
Alok-Patel-1203 <alokvpatel1203@gmail.com>
2026-08-24 21:11:12 +0530

    Added first change to main.cpp

--- PART C AND D/main.cpp
+++ PART C AND D/main.cpp
@@ -5,6 +5,6 @@ int main() {
     Calculator calc;
     std::cout << "10 + 5 = " << calc.add(10, 5) << std::endl;
     std::cout << "10 - 5 = " << calc.subtract(10, 5) << std::endl;
-    std::cout << "ENDING LINE";
+    std::cout << "First Change!!" << std::endl;
     return 0;
 }

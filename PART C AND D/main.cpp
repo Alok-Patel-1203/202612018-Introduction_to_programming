a0a5b64e453f679d0e135c256c5ed794870ddab2
Alok-Patel-1203 <alokvpatel1203@gmail.com>
2026-08-24 21:14:42 +0530

    Updated text on feature branch

--- PART C AND D/main.cpp
+++ PART C AND D/main.cpp
@@ -5,6 +5,6 @@ int main() {
     Calculator calc;
     std::cout << "10 + 5 = " << calc.add(10, 5) << std::endl;
     std::cout << "10 - 5 = " << calc.subtract(10, 5) << std::endl;
-    std::cout << "First Change!!" << std::endl;
+    std::cout << "Branch Changed Finished!!" << std::endl;
     return 0;
 }
